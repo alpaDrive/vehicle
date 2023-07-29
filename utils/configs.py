267ebd6abd857a59ff9d2172c6cae825f7534c66
vishalkrishnads <321vishalds@gmail.com>
2023-07-29 12:04:19 +0530

    Improved setup process

--- utils/configs.py
+++ utils/configs.py
@@ -7,5 +7,6 @@ PROTOCOLS = {
     'https': 'https',
     'ws': 'ws',
     'wss': 'wss'
-}
-SERVER_URL = '://alpadrive.selseus.com'
+} # just the basic web protocols
+SERVER_URL = '://alpadrive.selseus.com' # change to locahost:PORT for dev
+BUTTON = 21 # change to your desired pin number

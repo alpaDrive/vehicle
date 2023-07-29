a83a47cacb560f4e3e589b049b1cc5a60dfeebe9
Vishal <321vishalds@gmail.com>
2023-07-29 13:22:22 +0530

    Fixed #3
    * Improved setup UX
    
    * Improved docs
    
    * Optimized UX
    
    * Added circuit diagram
    
    * Improved setup process
    
    * Removed vague fuel counter
    
    * Converted power to threading as per #3
    
    * Fixed #6

--- utils/auth.py
+++ utils/auth.py
@@ -4,6 +4,8 @@
     This'll stay until we figure out a better way.
 '''
 import keyring
+import keyring.backend
+from keyrings.alt.file import PlaintextKeyring
 
 def is_authenticated():
     return keyring.get_password("alpaDrive", "vehicle") is not None
@@ -12,5 +14,10 @@ def get_creds():
     creds = keyring.get_password("alpaDrive", "vehicle")
     return creds if creds is not None else ""
 
-def set_creds(value):
+def set_creds(value, encrypt=False):
+    if encrypt:
+        # We can't use the default encrypted backend as that keeps asking for an encryption password in the console
+        # which is not viable for simplicity to the end user. ofc this is the same as using a text file but :(
+        keyring.set_keyring(PlaintextKeyring())
+
     keyring.set_password("alpaDrive", "vehicle", value)
